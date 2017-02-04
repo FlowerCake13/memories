88f48c4e3cbd376d644358f3f994c72974c294ab
FlowerCake13 <lminami23@punahou.edu>
2017-02-04 11:55:51 -1000

    Add files via upload
    picture and flipps

---
+++ js/app.js
@@ -0,0 +1,7 @@
+console.log("faweuhefwhuiuhiweff")
+console.log(document.getElementById('wrapper').childNodes)
+
+var cards = document.getElementById('wrapper').childNodes;
+cards [3].src = "assets/cake.png";
+console.log(cards[3]);
+// document.getElementById('wrapper').childNode[3].src = "assets/cake.png"
