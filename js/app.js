1742a6cfa6bf2f4d5e4de3d6f5caff4837340f80
FlowerCake13 <lminami23@punahou.edu>
2017-02-08 17:32:35 -1000

    Did JS.
     made cake go on two cards at random. brocoli on the rest

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
-console.log("faweuhefwhuiuhiweff")
-console.log(document.getElementById('wrapper').childNodes)
+function putPicOnCard() {
+	console.log(document.getElementsByTagName('img'));
+	var imgElement = document.getElementsByTagName('img');
+	//imgElement[1].src = '../assets/brocoli.png'
 
-var cards = document.getElementById('wrapper').childNodes;
-cards [3].src = "assets/cake.png";
-console.log(cards[3]);
-// document.getElementById('wrapper').childNode[3].src = "assets/cake.png"
+	var i;
+	for (i = 0; i < imgElement.length; i++){
+		imgElement[i].src = '../assets/brocoli.png';
+
+	}
+	var randomNum1 = Math.floor(Math.random() * (imgElement.length - 0)) + 0;
+	var randomNum2 = Math.floor(Math.random() * (imgElement.length - 0)) + 0;
+	console.log(randomNum1);
+	console.log(randomNum2);
+	if (randomNum1 === randomNum2) {
+		randomNum2 = Math.floor(Math.random() * (imgElement.length - 0)) + 0;
+		console.log(randomNum2);
+	}
+	imgElement[randomNum1].src = '../assets/cake.png';
+	imgElement[randomNum2].src = '../assets/cake.png';
+
+}
+
+putPicOnCard();
